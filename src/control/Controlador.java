0b4fde0e8ee75b61fa08d558e316ddbc9a9b4faa
Oskar32327 <72250644+Oskar32327@users.noreply.github.com>
2021-04-13 10:14:34 +0200

    Initial commit

---
+++ src/control/Controlador.java
@@ -0,0 +1,35 @@
+package control;
+
+import modelo.Coordenada;
+import modelo.GestionDatos;
+import utiles.RespuestaColocacion;
+
+public class Controlador {
+	private GestionDatos gestion;
+
+	public Controlador() {
+		super();
+		gestion = new GestionDatos();
+	}
+	
+	//void no es
+	public RespuestaColocacion realizarJugada(String posicion){
+		return this.gestion.realizarJugada(new Coordenada(posicion));
+	}
+
+	public String getTipoName() {
+		return this.gestion.getTipoActualName();
+	}
+
+	public String getTipoAnteriorName() {
+		return this.gestion.getTipoAnteriorName();
+	}
+	public RespuestaColocacion getIf3NRaya() {
+		return this.gestion.isTresEnRaya();
+	}
+
+//	public String getErrorName() {
+//		return this.gestion.getErrorActualName();
+//	}
+
+}
